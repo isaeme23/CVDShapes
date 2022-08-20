13d23836d63fb9459b392c8f5741ccc6b6bdf97d
unknown <isabella.manrique@labinfo.is.escuelaing.edu.co>
2022-08-20 10:01:27 -0500

    Entrega 1

---
+++ src/main/java/edu/eci/cvds/Patterns/App.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello ");
+        for (String x: args){
+            System.out.print(x + " ");
+        }
+    }
+}
