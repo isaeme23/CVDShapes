40a069a57ca9f170abafae50da0fed4bd5425660
isaeme23 <isabella.manrique@mail.escuelaing.edu.co>
2022-08-31 10:22:23 -0500

    Shapefactory Completo Ejecutados sin parametros qwerty, pentagon -> Parameter 'qwerty' is not a valid RegularShapeType Hexagon si pudo crearse

--- src/main/java/edu/eci/cvds/Patterns/Shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/Patterns/Shapes/ShapeFactory.java
@@ -1,11 +1,28 @@
 package edu.eci.cvds.Patterns.Shapes;
 
+import edu.eci.cvds.Patterns.Shapes.Concrete.Hexagon;
+import edu.eci.cvds.Patterns.Shapes.Concrete.Pentagon;
+import edu.eci.cvds.Patterns.Shapes.Concrete.Quadrilateral;
+import edu.eci.cvds.Patterns.Shapes.Concrete.Triangle;
+
 public class ShapeFactory{
 
-    public Shape create(RegularShapeType type){
-      switch (type){
-          case Triangle:
-
-      }
+    public Shape create(RegularShapeType type) {
+        Shape shape = null;
+        switch (type) {
+            case Triangle:
+                shape = new Triangle();
+                break;
+            case Quadrilateral:
+                shape = new Quadrilateral();
+                break;
+            case Hexagon:
+                shape = new Hexagon();
+                break;
+            case Pentagon:
+                shape = new Pentagon();
+                break;
+        }
+        return shape;
     }
 }
--- src/main/java/edu/eci/cvds/Patterns/Shapes/ShapeMain.java
+++ src/main/java/edu/eci/cvds/Patterns/Shapes/ShapeMain.java
@@ -1,12 +1,13 @@
 package edu.eci.cvds.Patterns.Shapes;
 public class ShapeMain {
     public static void main(String[] args) {
+        ShapeFactory example = new ShapeFactory();
         if (args == null || args.length != 1){
             System.err.println("Parameter of type RegularShapeType is required.");
             return;
         }try {
             RegularShapeType type = RegularShapeType.valueOf(args[0]);
-            Shape shape = ShapeFactory.create(type);
+            Shape shape = example.create(type);
             System.out.println(String.format("Successfully created a %s with %s sides.", type,shape.getNumberOfEdges()));
         } catch (IllegalArgumentException ex) {System.err.println("Parameter '" + args[0] + "' is not a valid RegularShapeType");
             return;
